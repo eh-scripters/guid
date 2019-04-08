ca3afe7b3e8e27fc704d73f9f01285669a51d314
Luna Flina <49369431+lunaflina@users.noreply.github.com>
2019-04-08 14:39:45 +0200

    newtags.js: Version update (20190408)

--- eh-guid-newtags.js
+++ eh-guid-newtags.js
@@ -4,7 +4,7 @@
 // @match https://e-hentai.org/tools.php*act=newtags*
 // @match http://e-hentai.org/tools.php*act=newtags*
 // @grant none
-// @version 20190331
+// @version 20190408
 // ==/UserScript==
 /*
 @licstart
