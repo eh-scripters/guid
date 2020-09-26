f106c3b1050cb7acc69831c42b8526870b37750c
PrincessKaguya <71513267+PrincessKaguya902@users.noreply.github.com>
2020-09-26 13:23:44 +0800

    tag-check-links.js
    Made by nasu_sensei. Add shortcut link(s) to tag checking pages via galleries.

---
+++ tag-check-links.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Tag Check Links
+// @version      0.1.3
+// @description  Add shortcut link(s) to tag checking pages via galleries
+// @author       nasu_sensei
+// @match        https://e-hentai.org/g/*
+// @match        https://exhentai.org/g/*
+// @grant        none
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js
+// ==/UserScript==
+ 
+(function() {
+    $(document).ready(function() {
+        var gid = window.location.href.split('/')[4];
+        var url = "https://e-hentai.org/tools.php?act=taglist&gid=" + gid;
+ 
+        $('#gmid #gd4').append('<a target="_blank" style="position:absolute;bottom:10px;right:19.5%;font-weight:bold;text-decoration:none" href="' + url + '">check</a>');
+    });
+})();
