99f1c324ba21ede9d1f605ad17f445696aaa8f05
Cipher-kun <cipher-kun@chin2.net>
2023-08-10 03:12:11 +0000

    add new script to add a link to tag check next to forum name in forum profile

---
+++ eh-guid-forum-check-link.user.js
@@ -0,0 +1,25 @@
+﻿// ==UserScript==
+// @name         Forum User Check Links
+// @version      0.1.0
+// @description  Add shortcut link(s) to tag checking page via forum profile
+// @author       nasu_sensei
+// @match        https://forums.e-hentai.org/index.php?showuser=*
+// @grant        none
+// ==/UserScript==
+
+"use strict;";
+
+(function () {
+  var uid;
+  var searchParams = new URLSearchParams(window.location.search);
+
+  if (searchParams.has('showuser')) {
+    uid = searchParams.get('showuser');
+  }
+
+  var usernameElement = document.getElementById('profilename').children[0];
+  usernameElement.innerHTML += '<small><a target="_blank" style="text-decoration:none" href="https://repo.e-hentai.org/tools.php?act=taglist&uid=' + uid + '"> ✔</small>';
+})();
+
+console.log("eh-guid-forum-user-check-link is active");
+
