fc5272e35189d18daffe8f2226a45ca5237e97e6
nasu <78793815+nasuht@users.noreply.github.com>
2024-03-14 19:26:11 +0000

    report-view: edit y pos of tooltip on hover to handle taglist scrolling

--- eh-guid-report-view.user.js
+++ eh-guid-report-view.user.js
@@ -358,8 +358,14 @@ find this file, see <http://www.gnu.org/licenses/>.
       var tool = tipBelow(elem, tip);
       // closures - i hate JS scopes
       elem.onmouseover = (function() {
-        var myTool = tool;
+        const myTool = tool;
+        const myElem = elem;
         return function (e) {
+          const ad = document.getElementById("spa");
+          const posTaglist = document.getElementById("taglist").getBoundingClientRect();
+          const adHeight = ad ? ad.getBoundingClientRect().height : 0;
+          const y = myElem.getBoundingClientRect().y + 18 + adHeight - posTaglist.y;
+          myTool.style.top = `${y}px`;
           myTool.style.display = "block";
         }
       })();
